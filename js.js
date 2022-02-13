3902f9e8592a64d0a4f97c4a965bec3c36019eb0
WilliamTrueheart <91620451+WilliamTrueheart@users.noreply.github.com>
2022-02-13 11:30:26 -0600

    Add files via upload

---
+++ js.js
@@ -0,0 +1,184 @@
+function growVoylesPuzzle() {
+    document.getElementById("voylesPuzzleUI").style.display = "flex";
+    document.getElementById("returnToPuzzle1").style.display = "flex";
+}
+
+function returnToPuzzle1() {
+    document.getElementById("voylesPuzzleUI").style.display = "none";
+    document.getElementById("brainBenders1UI").style.display = "none";
+    document.getElementById("brainBenders2UI").style.display = "none";
+    document.getElementById("returnToPuzzle1").style.display = "none";
+    document.getElementById("returnToPuzzle1B").style.display = "none";
+}
+
+function returnToPuzzle2() {
+    document.getElementById("brainBenders3UI").style.display = "none";
+    document.getElementById("brainBenders4UI").style.display = "none";
+    document.getElementById("returnToPuzzle2").style.display = "none";
+}
+
+function returnToLetterBoard() {
+    document.getElementById("voylesPuzzleDisplayA").style.display = "none";
+    document.getElementById("voylesPuzzleDisplayB").style.display = "none";
+    document.getElementById("voylesPuzzleDisplayC").style.display = "none";
+    document.getElementById("voylesPuzzleDisplayD").style.display = "none";
+    document.getElementById("voylesPuzzleDisplayE").style.display = "none";
+    document.getElementById("voylesPuzzleDisplayF").style.display = "none";
+    document.getElementById("voylesPuzzleDisplayG").style.display = "none";
+    document.getElementById("voylesPuzzleDisplayH").style.display = "none";
+    document.getElementById("voylesPuzzleDisplayI").style.display = "none";
+    document.getElementById("voylesPuzzleDisplayJ").style.display = "none";
+    document.getElementById("voylesPuzzleDisplayK").style.display = "none";
+    document.getElementById("voylesPuzzleDisplayL").style.display = "none";
+    document.getElementById("voylesPuzzleDisplayM").style.display = "none";
+    document.getElementById("voylesPuzzleDisplayN").style.display = "none";
+    document.getElementById("voylesPuzzleDisplayO").style.display = "none";
+    document.getElementById("voylesPuzzleDisplayP").style.display = "none";
+    document.getElementById("voylesPuzzleDisplayQ").style.display = "none";
+    document.getElementById("voylesPuzzleDisplayR").style.display = "none";
+    document.getElementById("voylesPuzzleDisplayS").style.display = "none";
+    document.getElementById("voylesPuzzleDisplayT").style.display = "none";
+    document.getElementById("voylesPuzzleDisplayU").style.display = "none";
+    document.getElementById("voylesPuzzleDisplayV").style.display = "none";
+    document.getElementById("voylesPuzzleDisplayW").style.display = "none";
+    document.getElementById("voylesPuzzleDisplayX").style.display = "none";
+    document.getElementById("voylesPuzzleDisplayY").style.display = "none";
+    document.getElementById("voylesPuzzleDisplayZ").style.display = "none";
+    document.getElementById("voylesPuzzleDisplaySecretMessage").style.display = "none";
+    document.getElementById("returnToLetterBoard").style.display = "none";
+}
+
+
+function growDisplayA() {
+    document.getElementById("voylesPuzzleDisplayA").style.display = "flex";
+    document.getElementById("returnToLetterBoard").style.display = "flex";
+}
+function growDisplayB() {
+    document.getElementById("voylesPuzzleDisplayB").style.display = "flex";
+    document.getElementById("returnToLetterBoard").style.display = "flex";
+}
+function growDisplayC() {
+    document.getElementById("voylesPuzzleDisplayC").style.display = "flex";
+    document.getElementById("returnToLetterBoard").style.display = "flex";
+}
+function growDisplayD() {
+    document.getElementById("voylesPuzzleDisplayD").style.display = "flex";
+    document.getElementById("returnToLetterBoard").style.display = "flex";
+}
+function growDisplayE() {
+    document.getElementById("voylesPuzzleDisplayE").style.display = "flex";
+    document.getElementById("returnToLetterBoard").style.display = "flex";
+}
+function growDisplayF() {
+    document.getElementById("voylesPuzzleDisplayF").style.display = "flex";
+    document.getElementById("returnToLetterBoard").style.display = "flex";
+}
+function growDisplayG() {
+    document.getElementById("voylesPuzzleDisplayG").style.display = "flex";
+    document.getElementById("returnToLetterBoard").style.display = "flex";
+}
+function growDisplayH() {
+    document.getElementById("voylesPuzzleDisplayH").style.display = "flex";
+    document.getElementById("returnToLetterBoard").style.display = "flex";
+}
+function growDisplayI() {
+    document.getElementById("voylesPuzzleDisplayI").style.display = "flex";
+    document.getElementById("returnToLetterBoard").style.display = "flex";
+}
+function growDisplayJ() {
+    document.getElementById("voylesPuzzleDisplayJ").style.display = "flex";
+    document.getElementById("returnToLetterBoard").style.display = "flex";
+}
+function growDisplayK() {
+    document.getElementById("voylesPuzzleDisplayK").style.display = "flex";
+    document.getElementById("returnToLetterBoard").style.display = "flex";
+}function growDisplayL() {
+    document.getElementById("voylesPuzzleDisplayL").style.display = "flex";
+    document.getElementById("returnToLetterBoard").style.display = "flex";
+}
+function growDisplayM() {
+    document.getElementById("voylesPuzzleDisplayM").style.display = "flex";
+    document.getElementById("returnToLetterBoard").style.display = "flex";
+}
+function growDisplayN() {
+    document.getElementById("voylesPuzzleDisplayN").style.display = "flex";
+    document.getElementById("returnToLetterBoard").style.display = "flex";
+}
+function growDisplayO() {
+    document.getElementById("voylesPuzzleDisplayO").style.display = "flex";
+    document.getElementById("returnToLetterBoard").style.display = "flex";
+}
+function growDisplayP() {
+    document.getElementById("voylesPuzzleDisplayP").style.display = "flex";
+    document.getElementById("returnToLetterBoard").style.display = "flex";
+}
+function growDisplayQ() {
+    document.getElementById("voylesPuzzleDisplayQ").style.display = "flex";
+    document.getElementById("returnToLetterBoard").style.display = "flex";
+}
+function growDisplayR() {
+    document.getElementById("voylesPuzzleDisplayR").style.display = "flex";
+    document.getElementById("returnToLetterBoard").style.display = "flex";
+}
+function growDisplayS() {
+    document.getElementById("voylesPuzzleDisplayS").style.display = "flex";
+    document.getElementById("returnToLetterBoard").style.display = "flex";
+}
+function growDisplayT() {
+    document.getElementById("voylesPuzzleDisplayT").style.display = "flex";
+    document.getElementById("returnToLetterBoard").style.display = "flex";
+}
+function growDisplayU() {
+    document.getElementById("voylesPuzzleDisplayU").style.display = "flex";
+    document.getElementById("returnToLetterBoard").style.display = "flex";
+}
+function growDisplayV() {
+    document.getElementById("voylesPuzzleDisplayV").style.display = "flex";
+    document.getElementById("returnToLetterBoard").style.display = "flex";
+}
+function growDisplayW() {
+    document.getElementById("voylesPuzzleDisplayW").style.display = "flex";
+    document.getElementById("returnToLetterBoard").style.display = "flex";
+}
+function growDisplayX() {
+    document.getElementById("voylesPuzzleDisplayX").style.display = "flex";
+    document.getElementById("returnToLetterBoard").style.display = "flex";
+}
+function growDisplayY() {
+    document.getElementById("voylesPuzzleDisplayY").style.display = "flex";
+    document.getElementById("returnToLetterBoard").style.display = "flex";
+}
+function growDisplayZ() {
+    document.getElementById("voylesPuzzleDisplayZ").style.display = "flex";
+    document.getElementById("returnToLetterBoard").style.display = "flex";
+}
+function growDisplaySecretMessage() {
+    document.getElementById("voylesPuzzleDisplaySecretMessage").style.display = "flex";
+    document.getElementById("returnToLetterBoard").style.display = "flex";
+}
+
+
+
+function growBrainBenders1() {
+    document.getElementById("brainBenders1UI").style.display = "flex";
+    document.getElementById("brainBenders2UI").style.display = "none";
+    document.getElementById("returnToPuzzle1B").style.display = "flex";
+}
+
+function growBrainBenders2() {
+    document.getElementById("brainBenders2UI").style.display = "flex";
+    document.getElementById("returnToPuzzle1B").style.display = "flex";
+    document.getElementById("brainBenders1UI").style.display = "none";
+}
+
+function growBrainBenders3() {
+    document.getElementById("brainBenders3UI").style.display = "flex";
+    document.getElementById("returnToPuzzle2").style.display = "flex";
+    document.getElementById("brainBenders4UI").style.display = "none";
+}
+
+function growBrainBenders4() {
+    document.getElementById("brainBenders4UI").style.display = "flex";
+    document.getElementById("returnToPuzzle2").style.display = "flex";
+    document.getElementById("brainBenders3UI").style.display = "none";
+}
